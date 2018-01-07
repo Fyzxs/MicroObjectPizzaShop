0b87a242abd572afbb3827e7d2becf17df8fd1d1
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-06 21:31:22 -0800

    Moved SentenceJoin to texts

---
+++ microObjectPizzaShop/Library/Texts/SentenceJoinTexts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MicroObjectPizzaShop.Library.Texts;
+
+namespace microObjectPizzaShop.Library.Texts {
+    public class SentenceJoinTexts : Text
+    {
+        private readonly List<IText> _toppings;
+
+        public SentenceJoinTexts(List<IText> toppings) => _toppings = toppings;
+
+        public override string String()
+        {
+            if (_toppings.Count == 0) return string.Empty;
+            if (_toppings.Count == 1) return _toppings.First().String();
+
+            string build = _toppings.First().String();
+            for (int idx = 1; idx < _toppings.Count - 1; idx++)
+            {
+                build += ", " + _toppings[idx].String();
+            }
+
+            return build + " and " + _toppings.Last().String();
+        }
+    }
+}
