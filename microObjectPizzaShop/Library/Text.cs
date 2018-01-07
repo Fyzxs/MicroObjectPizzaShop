083b00521d482bf879bcac960ca288ab2969e261
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-06 20:44:42 -0800

    Still using an interface

--- microObjectPizzaShop/Library/Text.cs
+++ microObjectPizzaShop/Library/Text.cs
@@ -1,9 +1,15 @@
 ﻿namespace microObjectPizzaShop.Library
 {
-    public abstract class Text
+    public abstract class Text : IText
     {
         public abstract string String();
 
         public bool IsEmpty() => String() == string.Empty;
+    }
+
+    public interface IText
+    {
+        string String();
+        bool IsEmpty();
     }
 }
