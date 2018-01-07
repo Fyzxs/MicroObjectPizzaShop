38f9eb9de6a7031091c58a85060d601645d597fe
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-06 20:15:27 -0800

    Added TextOf : IText

--- microObjectPizzaShop/PizzaShopTests.cs
+++ microObjectPizzaShop/PizzaShopTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace microObjectPizzaShop
 {
@@ -71,5 +72,27 @@ namespace microObjectPizzaShop
             if (string.IsNullOrWhiteSpace(_topping)) return "Personal pizza";
             return $"Personal pizza with {_topping}";
         }
+    }
+
+    public interface IText
+    {
+        string String();
+    }
+    public class TextOf : IText
+    {
+        private readonly IText _origin;
+
+        private class DelayedText : IText
+        {
+            private readonly Func<string> _origin;
+            public DelayedText(Func<string> origin) => _origin = origin;
+            public string String() => _origin();
+        }
+
+        public TextOf(string origin) : this(new DelayedText(() => origin)) { }
+
+        public TextOf(IText origin) => _origin = origin;
+
+        public string String() => _origin.String();
     }
 }
