fe58628c6bac8f3b157aaa5bd0bc7920bac07019
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-06 22:22:33 -0800

    Added Topping : ITopping to encapsulate name/price

---
+++ microObjectPizzaShop/Library/Texts/SentenceJoinToppings.cs
@@ -0,0 +1,28 @@
+﻿using MicroObjectPizzaShop;
+using MicroObjectPizzaShop.Library.Texts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace microObjectPizzaShop.Library.Texts
+{
+    public class SentenceJoinToppings : Text
+    {
+        private readonly List<ITopping> _toppings;
+
+        public SentenceJoinToppings(List<ITopping> toppings) => _toppings = toppings;
+
+        public override string String()
+        {
+            if (_toppings.Count == 0) return string.Empty;
+            if (_toppings.Count == 1) return _toppings.First().Name().String();
+
+            string build = _toppings.First().Name().String();
+            for (int idx = 1; idx < _toppings.Count - 1; idx++)
+            {
+                build += ", " + _toppings[idx].Name().String();
+            }
+
+            return build + " and " + _toppings.Last().Name().String();
+        }
+    }
+}
--- microObjectPizzaShop/PizzaShopTests.cs
+++ microObjectPizzaShop/PizzaShopTests.cs
@@ -28,7 +28,7 @@ namespace MicroObjectPizzaShop
         {
             //Arrange
             IPizza initial = new Pizza();
-            IPizza subject = initial.AddTopping(new TextOf("SomeTopping"));
+            IPizza subject = initial.AddTopping(new Topping(new TextOf("SomeTopping"), .1));
             //Act
             IText actual = subject.Description();
 
@@ -54,7 +54,7 @@ namespace MicroObjectPizzaShop
         {
             //Arrange
             IPizza initial = new Pizza();
-            IPizza subject = initial.AddTopping(new TextOf("SomeTopping"));
+            IPizza subject = initial.AddTopping(new Topping(new TextOf("SomeTopping"), .1));
 
             //Act
             IText val = subject.Price();
@@ -68,7 +68,9 @@ namespace MicroObjectPizzaShop
         {
             //Arrange
             IPizza initial = new Pizza();
-            IPizza subject = initial.AddTopping(new TextOf("SomeTopping")).AddTopping(new TextOf("OtherTopping"));
+            IPizza subject = initial
+                .AddTopping(new Topping(new TextOf("SomeTopping"), .1))
+                .AddTopping(new Topping(new TextOf("OtherTopping"), .1));
 
             //Act
             IText actual = subject.Price();
@@ -82,7 +84,9 @@ namespace MicroObjectPizzaShop
         {
             //Arrange
             IPizza initial = new Pizza();
-            IPizza subject = initial.AddTopping(new TextOf("SomeTopping")).AddTopping(new TextOf("OtherTopping"));
+            IPizza subject = initial
+                .AddTopping(new Topping(new TextOf("SomeTopping"), .1))
+                .AddTopping(new Topping(new TextOf("OtherTopping"), .1));
 
             //Act
             IText actual = subject.Description();
@@ -96,9 +100,10 @@ namespace MicroObjectPizzaShop
         {
             //Arrange
             IPizza initial = new Pizza();
-            IPizza subject = initial.AddTopping(new TextOf("SomeTopping"))
-                .AddTopping(new TextOf("OtherTopping"))
-                .AddTopping(new TextOf("Delicious"));
+            IPizza subject = initial
+                .AddTopping(new Topping(new TextOf("SomeTopping"), .1))
+                .AddTopping(new Topping(new TextOf("OtherTopping"), .1))
+                .AddTopping(new Topping(new TextOf("Delicious"), .1));
 
             //Act
             IText actual = subject.Description();
@@ -106,12 +111,50 @@ namespace MicroObjectPizzaShop
             //Assert
             actual.String().Should().Be("Personal pizza with SomeTopping, OtherTopping and Delicious");
         }
+
+        [TestMethod, TestCategory("unit")]
+        public void ShouldProvidePriceForMeatAndNonMeatTopping()
+        {
+            //Arrange
+            IPizza initial = new Pizza();
+            IPizza subject = initial
+                .AddTopping(new Topping(new TextOf("MeatTopping"), .15))
+                .AddTopping(new Topping(new TextOf("NonMeat"), .1));
+
+            //Act
+            IText val = subject.Price();
+
+            //Assert
+            val.String().Should().Be("$11.25");
+        }
     }
+
+    public class Topping : ITopping
+    {
+        private readonly IText _name;
+        private readonly double _percent;
+
+        public Topping(IText name, double percent)
+        {
+            _name = name;
+            _percent = percent;
+        }
+
+        public double Cost(double pizzaCost) => pizzaCost * _percent;
+        public IText Name() => _name;
+    }
+
+    public interface ITopping
+    {
+        double Cost(double pizzaCost);
+        IText Name();
+    }
+
     public interface IPizza
     {
-        IPizza AddTopping(IText topping);
         IText Description();
         IText Price();
+        IPizza AddTopping(ITopping topping);
     }
 
     public class Pizza : IPizza
@@ -119,24 +162,37 @@ namespace MicroObjectPizzaShop
         private static readonly IText PizzaType = new TextOf("Personal pizza");
         private static readonly IText Format = new TextOf("{0} with {1}");
 
-        private readonly List<IText> _toppings;
+        private readonly List<ITopping> _toppings;
 
-        public Pizza() : this(new List<IText>()) { }
-        private Pizza(List<IText> toppings) => _toppings = toppings;
+        public Pizza() : this(new List<ITopping>()) { }
+        private Pizza(List<ITopping> toppings) => _toppings = toppings;
 
-        public IPizza AddTopping(IText topping)
+        public IText Description()
+        {
+            if (!_toppings.Any()) return PizzaType;
+            List<IText> texts = new List<IText>
+            {
+                PizzaType,
+                new SentenceJoinToppings(_toppings)
+            };
+            return new FormatText(Format, texts.ToArray());
+        }
+
+        public IPizza AddTopping(ITopping topping)
         {
             _toppings.Add(topping);
             return new Pizza(_toppings);
         }
 
-        public IText Description()
+        public IText Price()
         {
-            if (!_toppings.Any()) return PizzaType;
-            List<IText> texts = new List<IText> { PizzaType, new SentenceJoinTexts(_toppings) };
-            return new FormatText(Format, texts.ToArray());
-        }
+            double cost = 0;
+            foreach (ITopping topping in _toppings)
+            {
+                cost += topping.Cost(9);
+            }
 
-        public IText Price() => new TextOf(( 9 + _toppings.Count * .9 ).ToString("C"));
+            return new TextOf(( 9 + cost ).ToString("C"));
+        }
     }
 }
