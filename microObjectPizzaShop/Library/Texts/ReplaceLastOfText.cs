b799994349a62146fc22b750ab514df2ae3c322c
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-08 01:20:26 -0800

    Split ReplaceLastOfText behavior into multiple classes

--- microObjectPizzaShop/Library/Texts/ReplaceLastOfText.cs
+++ microObjectPizzaShop/Library/Texts/ReplaceLastOfText.cs
@@ -5,28 +5,100 @@ namespace microObjectPizzaShop.Library.Texts
 {
     public class ReplaceLastOfText : IText
     {
+        private readonly IText _origin;
+
+        public ReplaceLastOfText(IText source, IText target, IText replace) :
+            this(new InsertText(new RemoveText(source, target), new LastIndexOf(source, target), replace))
+        { }
+        public ReplaceLastOfText(IText text) => _origin = text;
+
+        public string String() => _origin.String();
+    }
+
+    public class InsertText : IText
+    {
         private const int NotFound = -1;
         private readonly IText _source;
+        private readonly IInteger _index;
         private readonly IText _target;
-        private readonly IText _replace;
 
-        public ReplaceLastOfText(IText source, IText target, IText replace)
+        public InsertText(IText source, IInteger index, IText target)
+        {
+            _source = source;
+            _index = index;
+            _target = target;
+        }
+
+        public string String()
+        {
+            int lastIndexOf = _index.Value();
+            if (lastIndexOf == NotFound) return _source.String();
+
+            return _source.String().Insert(_index.Value(), _target.String());
+        }
+    }
+
+    public class RemoveText : IText
+    {
+        private const int NotFound = -1;
+
+        private readonly IText _source;
+        private readonly IInteger _lastIndexOf;
+        private readonly IInteger _lengthOf;
+
+        public RemoveText(IText source, IText target) :
+            this(source, new LastIndexOf(source, target), new LengthOf(target))
+        { }
+        public RemoveText(IText source, IInteger lastIndexOf, IInteger lengthOf)
+        {
+            _source = source;
+            _lastIndexOf = lastIndexOf;
+            _lengthOf = lengthOf;
+        }
+
+        public string String()
+        {
+            int lastIndexOf = _lastIndexOf.Value();
+            if (lastIndexOf == NotFound) return _source.String();
+
+            return _source.String().Remove(lastIndexOf, _lengthOf.Value());
+        }
+    }
+
+    public class LastIndexOf : IInteger
+    {
+        private readonly IText _source;
+        private readonly IText _target;
+
+        public LastIndexOf(IText source, IText target)
         {
             _source = source;
             _target = target;
-            _replace = replace;
         }
-        public string String()
-        {
-            //TODO: This method has a few string operations; can we text-ify?
-            string source = _source.String();
-            string target = _target.String();
 
-            int place = source.LastIndexOf(target, StringComparison.Ordinal);
+        public int Value() => _source.String().LastIndexOf(_target.String(), StringComparison.Ordinal);
+    }
 
-            return place == NotFound
-                ? source
-                : source.Remove(place, target.Length).Insert(place, _replace.String());
-        }
+    public class LengthOf : IInteger
+    {
+        private readonly IText _origin;
+
+        public LengthOf(IText origin) => _origin = origin;
+
+        public int Value() => _origin.String().Length;
+    }
+
+    public interface IInteger
+    {
+        int Value();
+    }
+
+    public class IntegerOf : IInteger
+    {
+        private readonly int _origin;
+
+        public IntegerOf(int origin) => _origin = origin;
+
+        public int Value() => _origin;
     }
 }
