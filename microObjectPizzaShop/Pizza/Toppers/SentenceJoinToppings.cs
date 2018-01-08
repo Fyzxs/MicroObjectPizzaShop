dfc536840a06d8f50de57aa920206b7d186f0c15
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-07 23:01:03 -0800

    Removing ctor logic

--- microObjectPizzaShop/Pizza/Toppers/SentenceJoinToppings.cs
+++ microObjectPizzaShop/Pizza/Toppers/SentenceJoinToppings.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
-using microObjectPizzaShop.Library.Texts;
+﻿using microObjectPizzaShop.Library.Texts;
 using MicroObjectPizzaShop.Library.Texts;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace microObjectPizzaShop.Pizza.Toppers
 {
@@ -9,10 +11,20 @@ namespace microObjectPizzaShop.Pizza.Toppers
         private readonly IText _origin;
 
         //TODO: Smelly - Logic in Ctor
-        public SentenceJoinToppings(IToppings toppings) : this(new SentenceJoinText(toppings.Select(t => t.Name()))) { }
-
+        public SentenceJoinToppings(IToppings toppings) : this(new ToppingRebaseToText(toppings)) { }
+        public SentenceJoinToppings(IEnumerable<IText> toppingNames) : this(new SentenceJoinText(toppingNames)) { }
         public SentenceJoinToppings(IText origin) => _origin = origin;
 
         public string String() => _origin.String();
+    }
+
+    public class ToppingRebaseToText : IEnumerable<IText>
+    {
+        private readonly IToppings _toppings;
+
+        public ToppingRebaseToText(IToppings toppings) => _toppings = toppings;
+        public IEnumerator<IText> GetEnumerator() => _toppings.Select(topping => topping.Name()).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
