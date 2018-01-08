c0e7ccc547a30216c5b8888a772bfb40fe7ca745
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-08 00:15:46 -0800

    fixing change to DelayedFormatText

---
+++ microObjectPizzaShop/Library/Texts/DelayedFormatText.cs
@@ -0,0 +1,30 @@
+﻿using MicroObjectPizzaShop.Library.Texts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace microObjectPizzaShop.Library.Texts
+{
+    public class DelayedFormatText : IDelayedFormatText
+    {
+        private readonly IText _format;
+        private readonly IList<IText> _args;
+        public DelayedFormatText(IText format, IText arg) : this(format, new List<IText> { arg }) { }
+        public DelayedFormatText(IText format) : this(format, new List<IText>()) { }
+
+        public DelayedFormatText(IText format, IList<IText> args)
+        {
+            _format = format;
+            _args = args;
+        }
+
+        public string String() => string.Format(_format.String(), _args.Select(t => t.String()).ToArray());
+
+        public IDelayedFormatText Add(IText arg)
+        {
+            List<IText> args = new List<IText>();
+            args.AddRange(_args);
+            args.Add(arg);
+            return new DelayedFormatText(_format, args);
+        }
+    }
+}
---
+++ microObjectPizzaShop/Library/Texts/IDelayedFormatText.cs
@@ -0,0 +1,8 @@
+﻿using MicroObjectPizzaShop.Library.Texts;
+
+namespace microObjectPizzaShop.Library.Texts {
+    public interface IDelayedFormatText : IText
+    {
+        IDelayedFormatText Add(IText arg);
+    }
+}
